e48c0016bb270b9fd9287cf83b22cd4cdfb86456
yohann <canyoh7@gmail.com>
2022-05-25 21:45:00 -0300

    Basic endpoints

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,42 @@
+class TasksController < ApplicationController
+  before_action :find_action
+  def index
+    render json: Task.all, status: :ok
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      render json: @task, status: :created
+    else
+      render json: @task.errors, status: :bad_request
+    end
+  end
+
+  def update
+    if @task.update task_params
+      render json: @task, status: :ok
+    else
+      render json: @task.errors, status: :bad_request
+    end
+  end
+
+  def destroy
+    if @task.destroy
+      head :no_content
+    else
+      head status: :bad_request
+    end
+  end
+  
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :archived, :status)
+  end
+
+  def find_action
+    @task = Task.find_by(id: params[:id])
+  end
+end
